0cf78854ef4730631d7d91f8e0ef57cb584d2ad9
aman47444 <aman47444>
2019-05-28 15:24:35 +0530

    edit

--- src/dashboard/Main.js
+++ src/dashboard/Main.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import ind from './img/flags/ind.jpg'
 import rsa from './img/flags/eng.jpg'
 import vs from './img/vs.png'
-
+import image from './img/image.png'
 class Main extends Component {
+    state={users:[{username:"aman"}]}
     render() {
         const height={
             height:'130px',
@@ -15,6 +16,17 @@ class Main extends Component {
         width:'150px'
        }
         return (
+            <React.Fragment>
+            <nav className="navbar bg-dark justify-content-between">
+            <div><a href="#" className="navbar-brand"><img src={image} className="pl-3" alt="newimage" style={{width:"250px",height:"60px"}}/></a></div>
+            <div class="nav-item dropdown mt-2 ml-3">
+              <button className="btn btn-danger nav-link dropdown-toggle" id="navbarDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{this.state.users[0].username}</button>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Home</a>
+              <a class="dropdown-item" href="#">Signout</a>
+              </div>
+            </div>
+           </nav>
             <div className="container text-center mb-3">
                 <h className="display-4 "><strong>Todays Match</strong></h>
                 <div className="row">
@@ -30,6 +42,7 @@ class Main extends Component {
                     </div>
                 </div>
             </div>
+            </React.Fragment>
         )
     }
 }
