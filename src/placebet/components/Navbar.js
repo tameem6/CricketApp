1e93b1a937a3f02ebbdcc9a8316fe1e06f2f991f
aman47444 <aman47444>
2019-05-29 15:45:00 +0530

    last

--- src/placebet/components/Navbar.js
+++ src/placebet/components/Navbar.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import "./static/Nav.css";
 import image from './static/image.png'
-import {Link} from 'react-router-dom';
-
 class Navbar extends Component
 {
     render()
@@ -10,12 +8,12 @@ class Navbar extends Component
         return(
                 <div className="menubar">
                     <nav className="navbar bg-dark justify-content-between">
-                    <div><Link to="/dashboard" className="navbar-brand"><img src={image} className="pl-3" alt="newimage" style={{width:"200px",height:"60px"}}/></Link></div>
+                    <div><a href="#" className="navbar-brand"><img src={image} className="pl-3" alt="newimage" style={{width:"200px",height:"60px"}}/></a></div>
                     <div className="nav-item dropdown mt-2 ml-3">
                     <button className="btn btn-danger nav-link dropdown-toggle" id="navbarDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{this.props.user}</button>
                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <Link className="dropdown-item" to='/dashboard'>Home</Link>
-                    <Link className="dropdown-item" to='/'>Signout</Link>
+                    <a className="dropdown-item" href="#">Home</a>
+                    <a className="dropdown-item" href="#">Signout</a>
                     </div>
                     </div>
                 </nav>
