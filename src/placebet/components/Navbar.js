5d8176c7803da4e5939634702935d3669a5e26a2
tameem6 <tameem6ansar@gmail.com>
2019-05-29 15:12:47 +0530

    Fixed links from a to Link

--- src/placebet/components/Navbar.js
+++ src/placebet/components/Navbar.js
@@ -10,12 +10,12 @@ class Navbar extends Component
         return(
                 <div className="menubar">
                     <nav className="navbar bg-dark justify-content-between">
-                    <div><Link to="/" className="navbar-brand"><img src={image} className="pl-3" alt="newimage" style={{width:"200px",height:"60px"}}/></Link></div>
+                    <div><Link to="/dashboard" className="navbar-brand"><img src={image} className="pl-3" alt="newimage" style={{width:"200px",height:"60px"}}/></Link></div>
                     <div className="nav-item dropdown mt-2 ml-3">
                     <button className="btn btn-danger nav-link dropdown-toggle" id="navbarDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{this.props.user}</button>
                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Home</a>
-                    <a className="dropdown-item" href="#">Signout</a>
+                    <Link className="dropdown-item" to='/dashboard'>Home</Link>
+                    <Link className="dropdown-item" to='/'>Signout</Link>
                     </div>
                     </div>
                 </nav>
