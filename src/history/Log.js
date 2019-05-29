61d4da1b88a4bb0dfff018a628919617e11a3600
santoshrnjn1898 <santosh.rnjn13@gmail.com>
2019-05-29 14:34:16 +0530

    Dashboard added

--- src/history/Log.js
+++ src/history/Log.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import axios from 'axios';
 import ENG from "./ENG.png";
 import AFGH from "./AFGH.png";
 import "./Logstyle.css";
-
 class Log extends Component {
   state = {
-    show: false,
-    player:[]
+    show: false
   };
   constructor() {
     super();
@@ -17,15 +14,7 @@ class Log extends Component {
     this.handleClose = this.handleClose.bind(this);
   }
 
-  componentDidMount(){
-    axios.get('https://cricapi.com/api/playerStats?apikey=me9l5ZLGRMeBZv55zZU44gAQgwN2&pid=35320')
-            .then(res => {
-              console.log(res);
-                this.setState({
-                    player: res.data
-                })
-            })
-  }
+ 
 
   handleClose() {
     this.setState({ show: false });
@@ -36,39 +25,21 @@ class Log extends Component {
   }
 
   render() {
-    const { player } = this.state;
-    const postList = player.length ? (
-      player.map(player => {
-          return (
-              <div className="post card" key={player.fullname}>
-
-                  <div className="card-content">
-
-                      <span className="card-title red-text ">
-                         <b> {player.battingStyle}</b>
-                      </span>
-
-                      <p>{player.profile}</p>
-                  </div>
-              </div>
-          )
-      })
-  ) : ( <div className="center">No Players Yet</div> )
-
     return (
       <div className="container">
         <div className="row">
           <div className=" col-md-4 ">
             <div className="card blue-grey darken-2">
               <div class="card-content white-text">
-                <span class="card-title center"><b>Match Highlights</b></span>
-                <div className="main-content">
+                <span class="card-title center"><b>Todays Match</b></span>
+                <div className="main-content justify-content-center">
                   <div className="flex1">
                     <div className="left-image">
                       <img src={AFGH} className="img-left" alt="flag left" />
                     </div>
                     <div className="team-name-left">Afgh</div>
                   </div>
+                  <div>V/S</div>
                   <div className="flex2">
                     <div className="team-name-right">ENG</div>
                     <div className="right-image">
@@ -79,18 +50,18 @@ class Log extends Component {
               </div>
               <div class="card-action act">
                 <Button onClick={this.handleShow}>
-                  STATS
+                  MATCH INFO
                 </Button>
 
                         <Modal show={this.state.show} onHide={this.handleClose}>
                           <Modal.Header>
-                            <Modal.Title>Winning Team Name</Modal.Title>
+                            <Modal.Title className="center">MATCH PREVIEW</Modal.Title>
                           </Modal.Header>
                           <Modal.Body>
-                           {postList}
+                           Match Information
                           </Modal.Body>
                           <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
+                            <Button variant="secondary " onClick={this.handleClose}>
                               Close
                             </Button>
                           </Modal.Footer>
