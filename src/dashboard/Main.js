d2bec07464b9b5f0adb58a44c770e4be0931d559
tameem6 <tameem6ansar@gmail.com>
2019-05-30 13:15:56 +0530

    Bg adjusted in dash

--- src/dashboard/Main.js
+++ src/dashboard/Main.js
@@ -3,6 +3,9 @@ import ind from './img/flags/ind.jpg'
 import rsa from './img/flags/eng.jpg'
 import vs from './img/vs.png'
 import Time  from "./Time";
+import Navbar from '../placebet/components/Navbar';
+import {Link} from 'react-router-dom';
+
 class Main extends Component {
     render() {
         const height={
@@ -19,11 +22,16 @@ class Main extends Component {
      
 
         return (
-            <div className="container text-center mb-3">
-                <h className="display-4 "><strong>Todays Match</strong></h>
+            <div>
+            <Navbar />
+            <div className="bg">
+            
+            <div className="container text-center mb-3" style={{paddingBottom:'472px'}}>
+                <h4 className="display-4 "><strong>Todays Match</strong></h4>
                 <div className="row">
                     <div className="col-lg-12">
-                        <div class="card text-white bg-light mb-3 " style={{borderRadius:"120px 120px 120px 120px"}}>
+                        <div className="card text-white bg-light mb-3 " style={{borderRadius:"120px 120px 120px 120px"}}>
+                            <Link to='/placebet'>
                             <div className="card-body p-0" >
                                 <img src={ind} style={height} className="float-left "  alt="hello"/>
                                 <img src={vs} style={mid} className="" alt="hello"/>
@@ -32,10 +40,14 @@ class Main extends Component {
 
                                 <h1 className="text-dark"><Time /></h1>                     
                             </div>
+                            </Link>
                         </div>
                     </div>
                 </div>
             </div>
+            </div>
+            </div>
+            
         )
     }
 }
