5ec6236722484da7299bdfe7840073ca2489f7e7
channu <channucs0009@gmail.com>
2019-05-31 14:43:00 +0530

    Updates

---
+++ precache-manifest.fbbbceb14cc9e68dabd4ec578f07e8f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7ebf761a337e2fcdf1753a9e0e319c",
+    "url": "/CricketApp/index.html"
+  },
+  {
+    "revision": "e51ed471e1d82d5e9ebe",
+    "url": "/CricketApp/static/css/main.172ee55d.chunk.css"
+  },
+  {
+    "revision": "d1f81f1e7eaa6cfca0ee",
+    "url": "/CricketApp/static/js/2.24f07d5b.chunk.js"
+  },
+  {
+    "revision": "e51ed471e1d82d5e9ebe",
+    "url": "/CricketApp/static/js/main.b6940bad.chunk.js"
+  },
+  {
+    "revision": "9fbde261f520c21d8ed0",
+    "url": "/CricketApp/static/js/runtime~main.dc2533c7.js"
+  },
+  {
+    "revision": "f289073663d62ccc1fb9930cbe4fb712",
+    "url": "/CricketApp/static/media/AFGH.f2890736.png"
+  },
+  {
+    "revision": "57a803c78e636f85498fd2a69b940a47",
+    "url": "/CricketApp/static/media/Afghanistan.57a803c7.jpg"
+  },
+  {
+    "revision": "8dba119f48f6400a9d4e62a620bc9eb8",
+    "url": "/CricketApp/static/media/Bangladesh.8dba119f.jpg"
+  },
+  {
+    "revision": "77a116b9d37827644db2d5123bd2b65f",
+    "url": "/CricketApp/static/media/ENG.77a116b9.png"
+  },
+  {
+    "revision": "55e1acd372bd48364748a29d06f7ccb9",
+    "url": "/CricketApp/static/media/India.55e1acd3.jpg"
+  },
+  {
+    "revision": "2e809818c88945e64ac75acfb6cd9b00",
+    "url": "/CricketApp/static/media/New Zealand.2e809818.jpg"
+  },
+  {
+    "revision": "69aa3a42324a1760438b642219e67206",
+    "url": "/CricketApp/static/media/West Indies.69aa3a42.jpg"
+  },
+  {
+    "revision": "894c0cae9d99bd1613687a5f3f75b825",
+    "url": "/CricketApp/static/media/aus.894c0cae.jpg"
+  },
+  {
+    "revision": "4b0c04c5467f8caa56f61267400a5183",
+    "url": "/CricketApp/static/media/bg-01.4b0c04c5.png"
+  },
+  {
+    "revision": "f633ac851a2fe2a3b967d5b10314a8db",
+    "url": "/CricketApp/static/media/vs.f633ac85.png"
+  }
+]);
