e9c8689b19d24e61dff1801e88b17b1e4130b25d
channu <channucs0009@gmail.com>
2019-05-31 13:43:10 +0530

    matches from api dynamic

--- src/dashboard/Main.js
+++ src/dashboard/Main.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
-import ind from './img/flags/ind.jpg'
-import rsa from './img/flags/eng.jpg'
+
 import vs from './img/vs.png'
 import Time  from "./Time";
 import Navbar from '../placebet/components/Navbar';
 import {Link} from 'react-router-dom';
 
+
+
 class Main extends Component {
+    state={
+        matches:[]
+    }
+    componentDidMount() {
+        const  url="http://192.168.0.142:8080/getFixture"
+          fetch(url)
+            .then(response => response.json())
+            .then(data =>this.setState({matches:data.value}))
+            .catch(error=>console.log("error in loading data->"+error));
+        }
     render() {
+           
         const height={
-            height:'130px',
+            height:'140px',
             borderRadius:"90px 90px 90px 90px",
           // transform:'rotateY(180deg)'
         }
@@ -17,6 +29,33 @@ class Main extends Component {
         height:'70px',
         width:'150px'
        }
+      
+        const matches=this.state.matches;
+        const result= matches.length ?(matches.map(match =>{
+
+            return(
+                <div className="col-lg-12" key={match.id}>
+                        <div className="card text-white bg-light mb-2 " style={{borderRadius:"120px 120px 120px 120px"}}>
+                            <Link to='/placebet'>
+                            <div className="card-body p-0" >
+                           
+                                <img src={require( "./img/flags/"+match.teama+".jpg")} style={height} className="float-left" 
+                                data-toggle="tooltip" title={match.teama} alt={match.teama}/>
+                               
+                                <img src={vs} style={mid} className="" alt="v/s"/>
+                                
+                                <img src={require( "./img/flags/"+match.teamb+".jpg")}  style={height} className="float-right"
+                                 data-toggle="tooltip" title={match.teamb} alt={match.teama}/>
+                            
+
+                                <h1 className="text-dark"><strong><Time time={match.gametime}/></strong></h1>                     
+                            </div>
+                            </Link>
+                        </div>
+                    </div>
+            )
+    })):(<h1>loading....</h1>)
+      
        
       
      
@@ -25,26 +64,14 @@ class Main extends Component {
             <div>
             <Navbar />
             <div className="bg">
-            
             <div className="container text-center mb-3" style={{paddingBottom:'472px'}}>
-                <h4 className="display-4 "><strong>Todays Match</strong></h4>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="card text-white bg-light mb-3 " style={{borderRadius:"120px 120px 120px 120px"}}>
-                            <Link to='/placebet'>
-                            <div className="card-body p-0" >
-                                <img src={ind} style={height} className="float-left "  alt="hello"/>
-                                <img src={vs} style={mid} className="" alt="hello"/>
-                                <img src={rsa} style={height} className="float-right" alt="hello"/>
-
-
-                                <h1 className="text-dark"><Time /></h1>                     
-                            </div>
-                            </Link>
-                        </div>
+                    <h4 className="display-4 "><strong>Todays Match</strong></h4>
+                    <div className="row">
+                       {
+                           result
+                       }
                     </div>
                 </div>
-            </div>
             </div>
             </div>
             
--- src/dashboard/Time.js
+++ src/dashboard/Time.js
@@ -1,58 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Time extends Component {
-    state={
-        time:'2019-05-20 15:01:10'
-     
-    }
+    state={}
+
+componentWillMount(){
+    this.setState({time:this.props.time})
+}
 
 componentDidMount(){
     let time;
    
-
-      const startTime = () => {
-    //          let today = new Date();
-    //          let h = today.getHours();
-    //          let m = today.getMinutes();
-    //          let s = today.getSeconds();
-    //  // add a zero in front of numbers<10
-    //                 if (m < 10) {
-    //                     m = "0" + m;
-    //                     }
-    //                     if (s < 10) {
-    //                         s = "0" + m;
-    //                         }
-             
-    //          time= h + ":" + m + ":" + s
-
-       
-
+    const startTime = () => {
     var endDate = new Date(this.state.time);
-
     var startdate = new Date(endDate);
-    
-  
-
+   
     var time1 = new Date( startdate.getTime() - 1000 * 1 );
             time=time1.toString();
          time=time.split("GMT+0530 (India Standard Time)"); 
-         console.log(time);
-
+         this.setState({time:time})
 }
-
    startTime();
-
-  
-   setInterval(() => {
-    startTime();
-    this.setState({time:time}) ;
-       }, 1000);
+//    setInterval(() => {
+//     startTime();
+//     this.setState({time:time}) ;
+//        }, 1000);
 }
-
-
-    render() {
-       
-
+    render() {      
         return (
             <div>
                 {this.state.time}
